24cb91b59caddfc1c1fe5bf81502dae4202ce60a
amazonAWS-tutorial <67293996+amazonAWS-tutorial@users.noreply.github.com>
2020-06-23 11:04:27 +0200

    first commit

---
+++ amazon-ecs-nodejs-microservices-bgsierra/2-containerized/services/api/server.js
@@ -0,0 +1,58 @@
+const app = require('koa')();
+const router = require('koa-router')();
+const db = require('./db.json');
+
+// Log requests
+app.use(function *(next){
+  const start = new Date;
+  yield next;
+  const ms = new Date - start;
+  console.log('%s %s - %s', this.method, this.url, ms);
+});
+
+router.get('/bgsierra/users-bgsierra', function *(next) {
+  this.body = db.users;
+});
+
+router.get('/bgsierra/users-bgsierra/:userId', function *(next) {
+  const id = parseInt(this.params.userId);
+  this.body = db.users.find((user) => user.id == id);
+});
+
+router.get('/bgsierra/threads-bgsierra', function *() {
+  this.body = db.threads;
+});
+
+router.get('/bgsierra/threads-bgsierra/:threadId', function *() {
+  const id = parseInt(this.params.threadId);
+  this.body = db.threads.find((thread) => thread.id == id);
+});
+
+router.get('/bgsierra/posts-bgsierra', function *() {
+  this.body = db.posts;
+});
+
+router.get('/bgsierra/posts-bgsierra/in-thread/:threadId', function *() {
+  const id = parseInt(this.params.threadId);
+  this.body = db.posts.filter((post) => post.thread == id);
+});
+
+router.get('/bgsierra/posts-bgsierra/by-user/:userId', function *() {
+  const id = parseInt(this.params.userId);
+  this.body = db.posts.filter((post) => post.user == id);
+});
+
+router.get('/bgsierra/', function *() {
+  this.body = "API ready to receive requests";
+});
+
+router.get('/', function *() {
+  this.body = "Ready to receive requests";
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.listen(3000);
+
+console.log('Worker started');
